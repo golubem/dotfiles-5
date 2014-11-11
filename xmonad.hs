8b37df1ee2151da580cc55904659e1cd8736bf5c
mathcrosp <mathcrosp@yandex.ru>
2014-11-11 22:17:00 +0500

    some changes

--- xmonad.hs
+++ xmonad.hs
@@ -24,14 +24,14 @@ myBorderColor  = "#363636"
 myFocusedColor = "#b8b8b8"
 
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = ["1:main", "2:misc", "3:misc", "4:mail", "5:web", "6:full", "7:vbox", "8:misc", "9:misc"]
+myWorkspaces = map show [1..9]
 
-myLayoutHook = 
-    onWorkspace (myWorkspaces !! 0 ) ( full ||| tiled ||| fullSp ||| tiledSp ) $
-    onWorkspace (myWorkspaces !! 4 ) ( full ||| fullSp ) $
-    onWorkspace (myWorkspaces !! 5 ) ( full ||| fullSp ) $
-    onWorkspace (myWorkspaces !! 6 ) ( full ||| tiled ||| fullSp ||| tiledSp ) $
-    full ||| tiled ||| fullSp ||| tiledSp
+myLayoutHook =
+    onWorkspace (myWorkspaces !! 0 ) ( fullSp ||| tiledSp ||| full ||| tiled) $
+    onWorkspace (myWorkspaces !! 4 ) ( fullSp ||| full ) $
+    onWorkspace (myWorkspaces !! 5 ) ( fullSp ||| full ) $
+    onWorkspace (myWorkspaces !! 6 ) ( fullSp ||| tiledSp ||| full ||| tiled) $
+    fullSp ||| tiledSp ||| full ||| tiled
     where
     tiled           = spacing 0 $ Tall master delta ratio
     tiledSp         = spacing 10 $ Tall master delta ratio
@@ -75,29 +75,16 @@ main = do
       , (( myModMask, xK_o ), spawn "okteta")
       , (( myModMask .|. shiftMask , xK_x ), spawn "gvim")
       , (( myModMask .|. shiftMask , xK_f ), spawn "pidgin")
+      , (( myModMask .|. shiftMask , xK_p ), spawn "amarok")
       , (( myModMask .|. shiftMask , xK_t ), spawn "dolphin")
       , (( myModMask .|. shiftMask , xK_b ), spawn "firefox")
       , (( myModMask .|. shiftMask , xK_w ), spawn "wireshark")
-      , (( myModMask .|. shiftMask , xK_s ), spawn "kdevelop")
       , (( myModMask .|. shiftMask , xK_o ), spawn "VirtualBox")
       , (( myModMask .|. shiftMask , xK_d ), spawn "monodevelop")
       , (( myModMask .|. shiftMask , xK_m ), spawn "thunderbird")
       , (( myModMask .|. shiftMask , xK_i ), spawn "/home/mathcrosp/.ida651/idaq")
       , (( myModMask .|. shiftMask , xK_n ), spawn "konsole -e bash -c alsamixer")
-      , (( myModMask .|. shiftMask , xK_p ), spawn "konsole -e bash -c ncmpcpp")
       , (( myModMask .|. shiftMask , xK_v ), spawn "konsole -e bash -c vifm")
-      -- mpd
-      , (( myModMask, xK_Left ), spawn "mpc prev")
-      , (( myModMask, xK_Right ), spawn "mpc next")
-      , (( myModMask, xK_Down ), spawn "mpc toggle")
-      , (( myModMask .|. shiftMask , xK_Left ), spawn "mpc seek -00:00:05")
-      , (( myModMask .|. shiftMask , xK_Right ), spawn "mpc seek +00:00:05")
-      -- volume settings
-      , (( controlMask .|. shiftMask , xK_F2 ), spawn "amixer set Master 10%-")
-      , (( controlMask .|. shiftMask , xK_F3 ), spawn "amixer set Master 10%+")
-      , (( controlMask, xK_F2 ), spawn "amixer set Master 5%-")
-      , (( controlMask, xK_F3 ), spawn "amixer set Master 5%+")
-      , (( controlMask, xK_F1 ), spawn "amixer set Speaker toggle")
       ]
       `additionalKeysP`
       [ ("M-z", spawn "konsole")
