cfdf3bc6271271d71ed181af2cc18093a9dff932
mathcrosp <mathcrosp@yandex.ru>
2014-11-24 16:45:36 +0000

    replace konsole with urxvt

--- xmonad.hs
+++ xmonad.hs
@@ -18,7 +18,7 @@ import XMonad.Util.Run (spawnPipe)
 import System.IO
 
 myModMask      = mod4Mask
-myTerminal     = "konsole"
+myTerminal     = "urxvt"
 myBorderWidth  = 5
 myBorderColor  = "#363636"
 myFocusedColor = "#b8b8b8"
@@ -27,29 +27,28 @@ myWorkspaces :: [WorkspaceId]
 myWorkspaces = map show [1..9]
 
 myLayoutHook =
-    onWorkspace (myWorkspaces !! 0 ) ( fullSp ||| tiledSp ||| full ||| tiled) $
-    onWorkspace (myWorkspaces !! 4 ) ( fullSp ||| full ) $
-    onWorkspace (myWorkspaces !! 5 ) ( fullSp ||| full ) $
-    onWorkspace (myWorkspaces !! 6 ) ( fullSp ||| tiledSp ||| full ||| tiled) $
-    fullSp ||| tiledSp ||| full ||| tiled
+    onWorkspace (myWorkspaces !! 0 ) ( full ||| tiled ||| fullSp ) $
+    onWorkspace (myWorkspaces !! 4 ) ( full ||| fullSp ) $
+    onWorkspace (myWorkspaces !! 5 ) ( full ||| fullSp ) $
+    onWorkspace (myWorkspaces !! 6 ) ( full ||| tiled ||| fullSp ) $
+    full ||| tiled ||| fullSp
     where
     tiled           = spacing 0 $ Tall master delta ratio
-    tiledSp         = spacing 10 $ Tall master delta ratio
     defaultTall     = ResizableTall 1 (1/100) (1/2) []
     master          = 1
     ratio           = 1/2
     delta           = 1/100
     full            = spacing 0 $ Full
-    fullSp          = spacing 10 $ Full
+    fullSp          = spacing 40 $ Full
 
 
 myManageHook = manageDocks <+> compHook <+> manageHook defaultConfig
     where compHook = composeAll
-                 [ className =? "Firefox"        --> doShift "5:web"
-                 , resource  =? "feh"            --> doShift "6:full"
-                 , className =? "Pidgin"         --> doShift "4:mail"
-                 , className =? "Thunderbird"    --> doShift "4:mail"
-                 , className =? "VirtualBox"     --> doShift "7:vbox"
+                 [ className =? "Firefox"        --> doShift "5"
+                 , resource  =? "feh"            --> doShift "6"
+                 , className =? "Pidgin"         --> doShift "4"
+                 , className =? "Thunderbird"    --> doShift "4"
+                 , className =? "VirtualBox"     --> doShift "7"
                  , className =? "Plasma"         --> doFloat
                  , className =? "Plasma-desktop" --> doFloat
                  ]
@@ -75,19 +74,20 @@ main = do
       , (( myModMask, xK_o ), spawn "okteta")
       , (( myModMask .|. shiftMask , xK_x ), spawn "gvim")
       , (( myModMask .|. shiftMask , xK_f ), spawn "pidgin")
-      , (( myModMask .|. shiftMask , xK_p ), spawn "amarok")
       , (( myModMask .|. shiftMask , xK_t ), spawn "dolphin")
       , (( myModMask .|. shiftMask , xK_b ), spawn "firefox")
       , (( myModMask .|. shiftMask , xK_w ), spawn "wireshark")
+      , (( myModMask .|. shiftMask , xK_p ), spawn "clementine")
       , (( myModMask .|. shiftMask , xK_o ), spawn "VirtualBox")
       , (( myModMask .|. shiftMask , xK_d ), spawn "monodevelop")
       , (( myModMask .|. shiftMask , xK_m ), spawn "thunderbird")
       , (( myModMask .|. shiftMask , xK_i ), spawn "/home/mathcrosp/.ida651/idaq")
-      , (( myModMask .|. shiftMask , xK_n ), spawn "konsole -e bash -c alsamixer")
-      , (( myModMask .|. shiftMask , xK_v ), spawn "konsole -e bash -c vifm")
+      , (( myModMask .|. shiftMask , xK_v ), spawn "urxvt -e bash -c vifm")
+      , (( myModMask .|. shiftMask , xK_n ), spawn "urxvt -e bash -c alsamixer")
+      , (( myModMask .|. shiftMask , xK_z ), spawn "urxvt -e bash -c 'tmux a -t 0'")
       ]
       `additionalKeysP`
-      [ ("M-z", spawn "konsole")
+      [ ("M-z", spawn "urxvt")
       ]
 
 -- ending
