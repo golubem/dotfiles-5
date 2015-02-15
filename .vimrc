65e87533d3b50d500ca43be6adb01891a929d42f
mathcrosp <mathcrosp@yandex.ru>
2015-02-16 01:32:41 +0500

    update

--- .vimrc
+++ .vimrc
@@ -1,8 +1,4 @@
 
-
-"""""""""""""""""""""""""""""" general
-
-
 " set path
 set path=.,,**
 
@@ -11,6 +7,9 @@ syn enable
 
 " enable lines numeration
 set number
+
+" enable mouse
+set mouse=a
 
 " width of '>>' and '<<' shifts
 set shiftwidth=4
@@ -31,7 +30,7 @@ set smartindent
 set wildmenu
 
 " completion popup
-set completeopt=longest,menuone
+set completeopt=menuone,menu,longest,preview
 
 " enable type-specific plugins loading
 filetype plugin on
@@ -39,54 +38,40 @@ filetype plugin on
 " make backspace work
 set backspace=indent,eol,start
 
-" make search case-insensitive
+" make search case-insensitive...
 set ignorecase
 
-" and make it case-sensitive when typing uppercase
+" ...and make it case-sensitive when typing uppercase
 set smartcase
 
-" remove unused bars
-set guioptions-=T
-set guioptions-=m
-set guioptions-=r
-set guioptions-=L
+" statusline
+set ruler
 
-" set font in gvim
-set guifont=PragmataPro\ 10.5
+" highlight current line
+set cursorline
 
-" set colorscheme (and window size for gvim)
-if has('gui_running')
-    colorscheme Tomorrow-Night-Blue
-    set lines=35 columns=120
-else
-    colorscheme elflord
-endif
+" set colorscheme
+colorscheme delek
+
+" lines above/below cursor
+set scrolloff=4
 
 " remove unwanted whitespaces
 autocmd BufWritePre * :%s/\s\+$//e
 
-" highlight long lines
-let w:m1=matchadd('Search', '\%<80v.\%>79v', -1)
-let w:m2=matchadd('ErrorMsg', '\%>79v.\+', -1)
+" make shortcuts
+map <F5> :!gcc -std=c99 -Wall -Werror -pedantic -O2 "%" -o "%:r" && ./"%:r" <Return>
+map <F6> :!g++ -std=c++11 -Wall -Werror -O2 "%" -o "%:r" && ./"%:r" <Return>
 
-" auto :set paste
-let &t_SI .= "\<Esc>[?2004h"
-let &t_EI .= "\<Esc>[?2004l"
-inoremap <special> <expr> <Esc>[200~ XTermPasteBegin()
-function! XTermPasteBegin()
-  set pastetoggle=<Esc>[201~
-  set paste
-  return ""
-endfunction
+" :set paste shortcut
+set pastetoggle=<F12>
 
 " keybindings
 let mapleader = ","
-nnoremap <A-j> 4jzz
-nnoremap <A-k> 4kzz
-nnoremap <Leader> :%s//<Left>
-nnoremap <C-b> :BufExplorer<Return>
-nnoremap <C-s> :w<Return>
-vnoremap <Leader> :s//<Left>
+nnoremap <Leader>s :%s//<Left>
+vnoremap <Leader>s :s//<Left>
+vnoremap < <gv
+vnoremap > >gv
 inoremap kj <Esc>
 inoremap <C-o> <C-x><C-o>
 inoremap <C-j> <Down>
@@ -96,48 +81,4 @@ nmap <C-j> <C-w>j
 nmap <C-k> <C-w>k
 nmap <C-h> <C-w>h
 nmap <C-l> <C-w>l
-
-
-"""""""""""""""""""""""""""""" plugins
-
-
-" vundle
-set nocompatible
-filetype off
-set rtp+=~/.vim/bundle/Vundle.vim
-
-call vundle#begin()
-Plugin 'bling/vim-airline'
-Plugin 'https://github.com/scrooloose/nerdtree.git'
-Plugin 'https://github.com/jlanzarotta/bufexplorer/'
-Plugin 'https://github.com/vim-ruby/vim-ruby'
-Plugin 'https://github.com/vim-scripts/OmniCppComplete'
-call vundle#end()
-filetype plugin indent on
-
-" airline
-if has('gui_running')
-        let g:airline_theme='tomorrow'
-        let g:airline#extensions#whitespace#enabled = 0
-        let g:airline_enable_bufferline=1
-        let g:airline_left_sep = ' '
-        let g:airline_right_sep = ' '
-        set laststatus=2
-endif
-
-" nerdtree
-hi NERDTreeDir ctermfg=darkblue
-
-" omnicppcomplete
-set tags+=~/.vim/tags/cpp
-let OmniCpp_NamespaceSearch = 1
-let OmniCpp_GlobalScopeSearch = 1
-let OmniCpp_ShowAccess = 1
-let OmniCpp_MayCompleteDot = 1 " autocomplete after .
-let OmniCpp_MayCompleteArrow = 1 " autocomplete after ->
-let OmniCpp_MayCompleteScope = 1 " autocomplete after ::
-let OmniCpp_DefaultNamespaces = ["std", "_GLIBCXX_STD"]
-au CursorMovedI,InsertLeave * if pumvisible() == 0|silent! pclose|endif
-set completeopt=menuone,menu,longest,preview
-
 
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 -- beginning
 
 import XMonad
+import XMonad.Config.Xfce
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
@@ -8,12 +9,14 @@ import XMonad.Layout
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.PerWorkspace (onWorkspace)
+
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import qualified XMonad.StackSet as W
 import XMonad.Util.Cursor
 import XMonad.Util.EZConfig
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.WorkspaceScreenshot
 import System.IO
 
 myModMask      = mod4Mask
@@ -23,7 +26,7 @@ myBorderColor  = "#363636"
 myFocusedColor = "#b8b8b8"
 
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = map show [1..9]
+myWorkspaces = map show [1..18]
 
 myLayoutHook =
     onWorkspace (myWorkspaces !! 0 ) ( full ||| tiled ||| fullSp ) $
@@ -44,7 +47,6 @@ myLayoutHook =
 myManageHook = manageDocks <+> compHook <+> manageHook defaultConfig
     where compHook = composeAll
                  [ className =? "Firefox"        --> doShift "5"
-                 , resource  =? "feh"            --> doShift "6"
                  , className =? "Pidgin"         --> doShift "4"
                  , className =? "VirtualBox"     --> doShift "7"
                  ]
@@ -52,16 +54,67 @@ myManageHook = manageDocks <+> compHook <+> manageHook defaultConfig
 myLogHook xmproc = dynamicLogWithPP $ compPP { ppOutput = hPutStrLn xmproc }
   where
     compPP = defaultPP {
-    ppHidden = xmobarColor "#a9acb6" ""
-    , ppCurrent = xmobarColor "#e2e2e2" "" . wrap "⠲" ""
-    , ppUrgent = xmobarColor "#a9acb6" "" . wrap "*" "*"
-    , ppLayout = xmobarColor "#ff0000" ""
+    ppHidden = xmobarColor "#919191" ""
+    , ppCurrent = xmobarColor "#fcfcfc" "" . wrap "" ""
+    , ppUrgent = xmobarColor "#919191" "" . wrap "*" "*"
+    , ppLayout = xmobarColor "#919191" ""
     , ppTitle = (\str -> "")
     , ppOrder = \(ws:_:t:_) -> [ws, t]
-    , ppSep = "<fc=#a9acb6> | </fc>"
+    , ppSep = "<fc=#fcfcfc> ] [ </fc>"
     }
 
+myKeys =
+      [ (( myModMask, xK_Print ), spawn "scrot")
+      , (( myModMask .|. shiftMask , xK_Print ), captureWorkspacesWhen defaultPredicate defaultHook horizontally)
+      , (( myModMask, xK_w ), spawn "feh --randomize --bg-fill /home/mathcrosp/pictures")
+      , (( controlMask, xK_F7 ), spawn "sleep 1 && xset dpms force off")
+      -- apps
+      , (( myModMask, xK_z ), spawn "urxvt")
+      , (( myModMask, xK_o ), spawn "okteta")
+      , (( myModMask .|. shiftMask , xK_i ), spawn "ida")
+      , (( myModMask .|. shiftMask , xK_g ), spawn "gimp")
+      , (( myModMask .|. shiftMask , xK_f ), spawn "pidgin")
+      , (( myModMask .|. shiftMask , xK_t ), spawn "thunar")
+      , (( myModMask .|. shiftMask , xK_b ), spawn "firefox")
+      , (( myModMask .|. shiftMask , xK_s ), spawn "kdevelop")
+      , (( myModMask .|. shiftMask , xK_o ), spawn "VirtualBox")
+      , (( myModMask .|. shiftMask , xK_n ), spawn "xfce4-mixer")
+      , (( myModMask .|. shiftMask , xK_d ), spawn "monodevelop")
+      , (( myModMask .|. shiftMask , xK_m ), spawn "thunderbird")
+      , (( myModMask .|. shiftMask , xK_x ), spawn "urxvt -e vim")
+      , (( myModMask .|. shiftMask , xK_h ), spawn "urxvt -e htop")
+      , (( myModMask .|. shiftMask , xK_v ), spawn "urxvt -e vifm")
+      , (( myModMask .|. shiftMask , xK_p ), spawn "urxvt -e ncmpcpp")
+      , (( myModMask .|. shiftMask , xK_z ), spawn "urxvt -e bash -c 'tmux a -t 0'")
+      -- mpd
+      , (( myModMask, xK_Left), spawn "mpc prev")
+      , (( myModMask .|. shiftMask, xK_Left), spawn "mpc seek -00:00:05")
+      , (( myModMask, xK_Down), spawn "mpc toggle")
+      , (( myModMask .|. shiftMask, xK_Right), spawn "mpc seek +00:00:05")
+      , (( myModMask, xK_Right), spawn "mpc next")
+      -- workspaces
+      , (( myModMask, xK_0), windows $ W.greedyView "10")
+      , (( shiftMask .|. myModMask, xK_0), windows $ W.shift "10")
+      , (( myModMask, xK_F1), windows $ W.greedyView "11")
+      , (( shiftMask .|. myModMask, xK_F1), windows $ W.shift "11")
+      , (( myModMask, xK_F2), windows $ W.greedyView "12")
+      , (( shiftMask .|. myModMask, xK_F2), windows $ W.shift "12")
+      , (( myModMask, xK_F3), windows $ W.greedyView "13")
+      , (( shiftMask .|. myModMask, xK_F3), windows $ W.shift "13")
+      , (( myModMask, xK_F4), windows $ W.greedyView "14")
+      , (( shiftMask .|. myModMask, xK_F4), windows $ W.shift "14")
+      , (( myModMask, xK_F5), windows $ W.greedyView "15")
+      , (( shiftMask .|. myModMask, xK_F5), windows $ W.shift "15")
+      , (( myModMask, xK_F6), windows $ W.greedyView "16")
+      , (( shiftMask .|. myModMask, xK_F6), windows $ W.shift "16")
+      , (( myModMask, xK_F7), windows $ W.greedyView "17")
+      , (( shiftMask .|. myModMask, xK_F7), windows $ W.shift "17")
+      , (( myModMask, xK_F8), windows $ W.greedyView "18")
+      , (( shiftMask .|. myModMask, xK_F8), windows $ W.shift "18")
+      ]
+
 main = do
+    initCapturing
     xmproc <- spawnPipe "/usr/bin/xmobar /home/mathcrosp/.xmonad/xmobarrc.hs"
     spawn "setxkbmap -layout 'us,ru' -option gpr:caps_toggle"
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig {
@@ -76,33 +129,6 @@ main = do
       , logHook            = myLogHook xmproc
       , focusFollowsMouse  = True
       , clickJustFocuses   = True
-    } `additionalKeys`
-      [ (( myModMask, xK_Print ), spawn "scrot")
-      , (( myModMask .|. shiftMask , xK_Print ), spawn "scrot -d 3")
-      , (( controlMask, xK_F7 ), spawn "sleep 1 && xset dpms force off")
-      -- apps
-      , (( myModMask, xK_o ), spawn "wxHexEditor")
-      , (( myModMask .|. shiftMask , xK_i ), spawn "ida")
-      , (( myModMask .|. shiftMask , xK_x ), spawn "gvim")
-      , (( myModMask .|. shiftMask , xK_g ), spawn "gimp")
-      , (( myModMask .|. shiftMask , xK_f ), spawn "pidgin")
-      , (( myModMask .|. shiftMask , xK_t ), spawn "thunar")
-      , (( myModMask .|. shiftMask , xK_b ), spawn "firefox")
-      , (( myModMask .|. shiftMask , xK_o ), spawn "VirtualBox")
-      , (( myModMask .|. shiftMask , xK_n ), spawn "xfce4-mixer")
-      , (( myModMask .|. shiftMask , xK_d ), spawn "monodevelop")
-      , (( myModMask .|. shiftMask , xK_v ), spawn "urxvt -e bash -c vifm")
-      , (( myModMask .|. shiftMask , xK_p ), spawn "urxvt -e bash -c ncmpcpp")
-      , (( myModMask .|. shiftMask , xK_z ), spawn "urxvt -e bash -c 'tmux a -t 0'")
-      -- mpd
-      , (( myModMask, xK_Left), spawn "mpc prev")
-      , (( myModMask .|. shiftMask, xK_Left), spawn "mpc seek -00:00:05")
-      , (( myModMask, xK_Down), spawn "mpc toggle")
-      , (( myModMask .|. shiftMask, xK_Right), spawn "mpc seek +00:00:05")
-      , (( myModMask, xK_Right), spawn "mpc next")
-      ]
-      `additionalKeysP`
-      [ ("M-z", spawn "urxvt")
-      ]
+    } `additionalKeys` myKeys
 
 -- ending
